fd98e6799ebe3befee740d3639b7be5823e53c35
nktnkt <ntopoev56@gmail.com>
2024-06-10 21:02:09 +0700

    Module 5, task 4 (readme and header)

--- module-5/task-4/files/chardev.c
+++ module-5/task-4/files/chardev.c
@@ -7,12 +7,7 @@
 #include <linux/kernel.h> 
 #include <linux/module.h> 
 #include <linux/poll.h> 
-
-/*  Prototypes - this would normally go in a .h file */ 
-static int device_open(struct inode *, struct file *); 
-static int device_release(struct inode *, struct file *); 
-static ssize_t device_read(struct file *, char __user *, size_t, loff_t *); 
-static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *); 
+#include "chardev.h"
 
 #define SUCCESS 0 
 #define DEVICE_NAME "chardev" /* Dev name as it appears in /proc/devices   */ 
---
+++ module-5/task-4/files/chardev.h
@@ -0,0 +1,9 @@
+#ifndef _CHARDEV_H_
+#define _CHARDEV_H_
+
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *);
+
+#endif
